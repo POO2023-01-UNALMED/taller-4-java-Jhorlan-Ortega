e8e88f593d21cbd7526a568909df8fe3c24c7534
Jhorlan-Ortega <124930448+Jhorlan-Ortega@users.noreply.github.com>
2023-03-07 07:50:20 -0500

    Intento_3

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -17,9 +17,7 @@ public class Asignatura {
         this("Sin nombre", codigoInterno, 0,tipo);
     }
 
-    public Asignatura(int codigoExterno,Tipo tipo) {
-        this("Sin nombre", 0, codigoExterno,tipo);
-    }
+
 
     public Asignatura(String nombre, Tipo tipo) {
         this(nombre, 0, 0,tipo);
@@ -43,9 +41,7 @@ public class Asignatura {
         this.codigoInterno = codigoInterno;
     }
 
-    public void cambiarDatos(int codigoExterno) {
-        this.codigoExterno = codigoExterno;
-    }
+
 
     public void cambiarDatos(String nombre) {
         this.nombre = nombre;
@@ -58,6 +54,6 @@ public class Asignatura {
     public void setCodigoInterno(double codigoInterno) {
         this.codigoInterno = (int) codigoInterno;
     }
-
+}
 
 }
